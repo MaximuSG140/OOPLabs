82cc43b0683482e608cdd6c4c18a28157f9413e9
MaximuSG140 <maximstepanov10092002@gmail.com>
2021-09-04 16:43:35 +0700

    Updated specification files
    Added almost all public methods to class RNA needed for task.

--- DNARNAContainer/DNARNAContainer/NuclBuffer.h
+++ DNARNAContainer/DNARNAContainer/NuclBuffer.h
@@ -7,6 +7,7 @@ public:
 	void AddNucl(Nucleotide newNucl);
 	void RemoveLast();
 	void Clear();
+	NuclStake GetStake();
 	NuclBuffer(int number, Nucleotide value);
 private:
 	NuclStake current = 0;
--- DNARNAContainer/DNARNAContainer/RNA.h
+++ DNARNAContainer/DNARNAContainer/RNA.h
@@ -1,13 +1,27 @@
-#include<vector>
-#include"Nucleotides.h"
+#include <vector>
+#include "Nucleotides.h"
+#include "NuclBuffer.h"
 #pragma once
 
 
 class RNA
 {
 public:
+	RNA();
+	RNA(int capacity);
+	RNA(int capacity, Nucleotide baseValue);
+	//RNA(RNA r);
+	~RNA();
+	RNA operator+ (RNA r);
+	RNA operator=  (RNA r);
+	RNA operator! ();
+	bool operator== (RNA r);
+	bool operator!= (RNA r);
+	bool IsComplimentary(RNA r);
+	RNA operator[](int index);
+	std::pair<RNA, RNA> Split(int index);
 private:
 	std::vector<NuclStake>storage;
-	NucleotideBuffer buffer;
+	NuclBuffer buffer;
 };
 
