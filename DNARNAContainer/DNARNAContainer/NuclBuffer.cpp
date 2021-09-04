1c8578d889e64ae2a572b187f334d83066eb79d5
MaximuSG140 <maximstepanov10092002@gmail.com>
2021-09-04 17:39:38 +0700

    Added implementation
    Implemented addition new Nucleotide to Nucleotide stack

--- DNARNAContainer/DNARNAContainer/NuclBuffer.cpp
+++ DNARNAContainer/DNARNAContainer/NuclBuffer.cpp
@@ -1,1 +1,7 @@
 #include "NuclBuffer.h"
+
+void NuclBuffer::AddNucl(Nucleotide newNucl)
+{
+	current &= (1 << (nuclAmmount << 1) - 1) << ((4 - nuclAmmount) << 1);
+	current += newNucl << ((3 - nuclAmmount) << 1);
+}
