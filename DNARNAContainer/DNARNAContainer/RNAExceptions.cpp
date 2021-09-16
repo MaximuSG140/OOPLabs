99ea255f733e8f83f07c41fd9e110da666658599
MaximuSG140 <maximstepanov10092002@gmail.com>
2021-09-16 20:47:05 +0700

    Updated exceptions
    Added correct what() override
    Added comparator for testing

--- DNARNAContainer/DNARNAContainer/RNAExceptions.cpp
+++ DNARNAContainer/DNARNAContainer/RNAExceptions.cpp
@@ -4,9 +4,30 @@
 char const* invalid_index_exception::what() const
 {
 	std::string msg;
-	msg += "Invalid index:\nMinimal allowed value:";
-	char* minIndexStr[32];
+	msg += "Invalid index:\nMinimal allowed value: ";
+	char* minIndexStr = new char[32];
 	_itoa_s(minimumIndex, minIndexStr, 32, 10);
 	msg += minIndexStr;
-	return "Invalid index:";
+	msg += "\nMaximal allowed value: ";
+	char* maxIndexStr = new char[32];
+	_itoa_s(maximumIndex, maxIndexStr, 32, 10);
+	msg += maxIndexStr;
+	msg += "\nUsed index: ";
+	char* indexStr = new char[32];
+	_itoa_s(index, indexStr, 32, 10);
+	msg += indexStr;
+	delete[] minIndexStr;
+	delete[] maxIndexStr;
+	delete[] indexStr;
+	char* cstringMsg = new char[msg.length()];
+	for(unsigned int i = 0; i < msg.length(); ++i)
+	{
+		cstringMsg[i] = msg[i];
+	}
+	return cstringMsg;
+}
+
+bool invalid_index_exception::operator==(const invalid_index_exception& e) const
+{
+	return index == e.index && minimumIndex == e.minimumIndex && maximumIndex == maximumIndex;
 }
--- DNARNAContainer/DNARNAContainer/RNAExceptions.h
+++ DNARNAContainer/DNARNAContainer/RNAExceptions.h
@@ -10,5 +10,6 @@ private:
 public:
 	char const* what() const override;
 	invalid_index_exception(unsigned int i, unsigned int min, unsigned int max) :exception(), index(i), maximumIndex(max), minimumIndex(min) {}
+	bool operator==(const invalid_index_exception& e)const;
 };
 
