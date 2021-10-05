b8fb284a7539ecfac99b9a3a92547d28551acb34
MaximuSG140 <maximstepanov10092002@gmail.com>
2021-10-05 22:32:51 +0700

    Changed constructor
    Now there is correcy checks

--- WorkFlowExecutor/Executor/Executor.cpp
+++ WorkFlowExecutor/Executor/Executor.cpp
@@ -1,12 +1,27 @@
 ﻿// Executor.cpp : Определяет функции для статической библиотеки.
 //
 #include "Executor.h"
+#include "FileSplitter.h"
 
 
-Executor::Executor(std::string fileName)
+Executor::Executor(const std::string& fileName)
 {
-	compiler = BlockReader(FileSplitter::GetBlockPart(fileName));
-	runner = NodeReader(FileSplitter::GetNodePart(fileName), compiler.ReadAllBlocks());
+	if(!FileSplitter::HasCorrectDescription(fileName))
+	{
+		throw std::exception();		//TODO: add a proper exception for this case
+	}
+	FileSplitter::GetBlockPart(fileName);
+	FileSplitter::GetNodePart(fileName);
+	if(!BlockReader::IsCorrect("BlockSequence.txt"))
+	{
+		throw std::exception();		//TODO: add a proper exception for this case
+	}
+	compiler = BlockReader("BlockSequence.txt");
+	if (NodeReader::IsCorrect("NodeSequence.txt", compiler.ReadAllBlocks()))
+	{
+		throw std::exception();		//TODO: add a proper exception for this case
+	}
+	runner = NodeReader("NodeSequence.txt", compiler.ReadAllBlocks());
 }
 
 void Executor::Run()
--- WorkFlowExecutor/Executor/Executor.h
+++ WorkFlowExecutor/Executor/Executor.h
@@ -1,12 +1,11 @@
 #pragma once
 #include "NodeReader.h"
 #include "BlockReader.h"
-#include "FileSplitter.h"
 
 class Executor
 {
 public:
-	Executor(std::string fileName);
+	Executor(const std::string& fileName);
 	void Run();
 private:
 	NodeReader runner;
