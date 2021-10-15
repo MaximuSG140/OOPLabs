e51f8a29faf3f11a1985cffffef6d8a8c5a7841f
MaximuSG140 <maximstepanov10092002@gmail.com>
2021-10-15 12:51:02 +0700

    Used functional decomposotion
    Added function thah gets operation enum and args and returns ptr to task specified in operation

--- WorkFlowExecutor/Executor/BlockReader.cpp
+++ WorkFlowExecutor/Executor/BlockReader.cpp
@@ -8,6 +8,51 @@
 #include "TaskGrep.h"
 #include "CompileTimeExceptions.h"
 
+Task* CreateTask(operations operation, const std::vector<std::string>& arguments, const int i)
+{
+	switch (operation)
+	{
+	case operations::read:
+		if (arguments.size() != 1)
+		{
+			throw invalid_argument_ammount(i + 1, arguments.size(), 1);
+		}
+		return new TaskReadFile(arguments[0]);
+	case operations::write:
+		if (arguments.size() != 1)
+		{
+			throw invalid_argument_ammount(i + 1, arguments.size(), 1);
+		}
+		return new TaskWriteFile(arguments[0]);
+	case operations::grep:
+		if (arguments.size() != 1)
+		{
+			throw invalid_argument_ammount(i + 1, arguments.size(), 1);
+		}
+		return new TaskGrep(arguments[0]);
+	case operations::replace:
+		if (arguments.size() != 2)
+		{
+			throw invalid_argument_ammount(i + 1, arguments.size(), 2);
+		}
+		return new TaskReplace(arguments[0], arguments[1]);
+	case operations::dump:
+		if (arguments.size() != 1)
+		{
+			throw invalid_argument_ammount(i + 1, arguments.size(), 1);
+		}
+		return new TaskDump(arguments[0]);
+	case operations::sort:
+		if (!arguments.empty())
+		{
+			throw invalid_argument_ammount(i + 1, arguments.size(), 0);
+		}
+		return new TaskSort();
+	}
+	return nullptr;
+}
+
+
 std::map<int, Task*> BlockReader::ReadAllBlocks(const std::vector<Block>& blocks)
 {
 	std::map<int, Task*> res;
@@ -21,51 +66,7 @@ std::map<int, Task*> BlockReader::ReadAllBlocks(const std::vector<Block>& blocks
 		{
 			throw invalid_assignment_sign(i + 1, blocks[i].assignment);
 		}
-		switch(blocks[i].operation)
-		{
-		case operations::read:
-			if(blocks[i].arguments.size() != 1)
-			{
-				throw invalid_argument_ammount(i + 1, blocks[i].arguments.size(), 1);	
-			}
-			res[blocks[i].number] = new TaskReadFile(blocks[i].arguments[0]);
-			break;
-		case operations::write:
-			if (blocks[i].arguments.size() != 1)
-			{
-				throw invalid_argument_ammount(i + 1, blocks[i].arguments.size(), 1);
-			}
-			res[blocks[i].number] = new TaskWriteFile(blocks[i].arguments[0]);
-			break;
-		case operations::grep:
-			if (blocks[i].arguments.size() != 1)
-			{
-				throw invalid_argument_ammount(i + 1, blocks[i].arguments.size(), 1);
-			}
-			res[blocks[i].number] = new TaskGrep(blocks[i].arguments[0]);
-			break;
-		case operations::replace:
-			if (blocks[i].arguments.size() != 2)
-			{
-				throw invalid_argument_ammount(i + 1, blocks[i].arguments.size(), 2);
-			}
-			res[blocks[i].number] = new TaskReplace(blocks[i].arguments[0], blocks[i].arguments[1]);
-			break;
-		case operations::dump:
-			if (blocks[i].arguments.size() != 1)
-			{
-				throw invalid_argument_ammount(i + 1, blocks[i].arguments.size(), 1);
-			}
-			res[blocks[i].number] = new TaskDump(blocks[i].arguments[0]);
-			break;
-		case operations::sort:
-			if (!blocks[i].arguments.empty())
-			{
-				throw invalid_argument_ammount(i + 1, blocks[i].arguments.size(), 0);
-			}
-			res[blocks[i].number] = new TaskSort();
-			break;
-		}
+		res[blocks[i].number] = CreateTask(blocks[i].operation, blocks[i].arguments, i);
 	}
 	return res;
 }
